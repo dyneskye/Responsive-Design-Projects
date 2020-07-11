21f75241613d3d4e387200f22044107538fcba31
dyneskye <dyneskye@gmail.com>
2020-07-11 14:59:59 +0100

    added beta day / night cycle

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -1,6 +1,42 @@
+$.fn.sayHi = function() {
+    this.html('Hi');
+    return this;
+  }
 
+$.fn.visibilityToggle = function() {
+        return this.css('opacity', (i, percent) => {
+        return (percent == 1) ? 0 : 1;
+    })
+};
 
-document.getElementById('welcome-header').onmouseover = function() {
-    document.getElementById('invisible').style.transition="opacity 500ms";
-    document.getElementById('invisible').style.opacity="100%";
+async function toggleSky() {
+    $('.blueSky, .stars, .twinkling').css('transition', 'opacity 2000ms');
+    $('.twinkling').css('visibility', 'hidden');
+    await $('.stars, .twinkling').visibilityToggle();
+    await $('.blueSky').visibilityToggle();
+    
+    /* if ($('.blueSky').css('opacity') == 1) { 
+      $('.stars, .twinkling').css('opacity', 1);
+      $('.blueSky').css('opacity', 0);
+    } else {
+      $('.stars, .twinkling').css('opacity', 0);
+      $('.blueSky').css('opacity', 1);
+    } */
 }
+
+$('#welcome-header').on('mouseover', () => {
+    document.getElementById('invisible').style.transition="opacity 2000ms";
+    document.getElementById('invisible').style.opacity="100%";
+    toggleSky();
+/*  $('#welcome-header').css('transition', 'ease-in-out');
+    $('#welcome-header').css('transition-duration', '1s');
+    $('#welcome-header').css('background', '#f5a361'); */
+})
+
+$('#aboutButton').on('click', (e) => {
+    document.getElementById('invisible').style.transition="opacity 2000ms";
+    document.getElementById('invisible').style.opacity="100%";
+/*  $('#welcome-header').css('transition', 'ease-in-out');
+    $('#welcome-header').css('transition-duration', '1s');
+    $('#welcome-header').css('background', '#f5a361'); */
+})
